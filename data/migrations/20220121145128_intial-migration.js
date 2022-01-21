4541aeb6f6c57288209240a0d13545026c9624c9
Laila Arkadan <lailaarkadan@Lailas-MacBook-Air.local>
2022-01-21 10:08:28 -0700

    working on required tables

--- data/migrations/20220121145128_intial-migration.js
+++ data/migrations/20220121145128_intial-migration.js
@@ -1,15 +1,59 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.up = function(knex) {
+    return knex.schema 
+     .createTable('projects', tbl => {
+      tbl.increments('project_id')
+      tbl.text('project_name' )
+          .notNullable()
+      tbl.text('project_description')
+      tbl.boolean('project_completed')
+          .defaultTo(false)
+          
+     })
   
-};
-
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
+      .createTable('resources', tbl => {
+          tbl.increments('resource_id')
+          tbl.text('resource_name')
+              .notNullable()
+              .unique()
+          tbl.text('resource_description')
+      })
   
-};
+      .createTable('tasks', tbl =>{
+          tbl.increments('task_id')
+          tbl.text('task_description')
+              .notNullable()
+          tbl.text('task_notes')
+          tbl.boolean('task_completed')
+              .defaultTo(false)
+              .unsigned()
+          tbl.integer('project_id')
+              .unsigned()
+              .notNullable()
+              .references('project_id')
+              .inTable('projects')
+              .onDelete('RESTRICT')
+              .onUpdate('RESTRICT');
+      })
+  
+      .createTable('project_resources', tbl =>{
+          tbl.increments('project_resources_id')
+          tbl.text('resource_description')
+          tbl.integer('resource_id')
+              .notNullable()
+              .unsigned()
+              .references('resource_id')
+              .inTable('resources')
+              .onDelete('RESTRICT')
+            
+      })
+  
+  };
+  
+  exports.down = function(knex) {
+      return knex.schema
+      .dropTableIfExists('project_resources')
+      .dropTableIfExists('tasks')
+      .dropTableIfExists('resources')
+      .dropTableIfExists('projects')
+      
+  };
